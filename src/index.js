6b309430f49cb1bab0fb35bb65d983bb00496a9a
Ian Campbell <iancam92@icloud.com>
2019-09-02 22:49:55 -0700

    put tests into a separate file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ const { assignDaysFactory, chooseFlowFactory } = require("./logic");
 const assignDays = assignDaysFactory(constantState);
 const chooseFlow = chooseFlowFactory({ assignDays });
 
-let op = [chooseFlow({ day: 1, questionCursor: 0 })];
-let last = op[op.length - 1];
-for (let x = 0; x < 10; x++) {
-  op.push(chooseFlow({ ...last, day: (last.day + 1) % 7 }));
-  last = op[op.length - 1];
-}
-console.log(JSON.stringify(op));
+module.exports = (request, response) => {
+  response.json(chooseFlow(request(chooseFlow(request.query))));
+  console.log(request.query);
+};
